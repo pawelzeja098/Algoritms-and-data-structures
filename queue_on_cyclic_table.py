6756172a0e2a38c8c494d9393621d11c79b57cbe
Pawel <pawel.zeja@infokam.kielce.pl>
2024-03-14 11:49:01 +0100

    skeleton

---
+++ queue_on_cyclic_table.py
@@ -0,0 +1,25 @@
+class Queue:
+    
+    def __init__(self,size = 5) -> None:
+        self.tab = [None for i in range(size)]
+    
+    def is_empty(self) -> bool:
+        pass
+
+    def peek(self):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def enqueue(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+def main():
+    queue = Queue()
+
+main()
